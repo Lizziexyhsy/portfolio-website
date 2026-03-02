54f00b7b080325bee89ad7d6a7f319ec84cdeb37
Lizziexyhsy <ahmadhakiem77@gmail.com>
2026-03-03 03:43:10 +0800

    update black white version portfolio

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
- console.log("Portfolio Loaded Successfully");
+// Active menu saat scroll
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 150;
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
+
+// Form validation
+const form = document.getElementById("contactForm");
+const message = document.getElementById("formMessage");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const text = document.getElementById("message").value;
+
+    if(name && email && text){
+        message.textContent = "Message sent successfully!";
+        form.reset();
+    } else {
+        message.textContent = "Please fill all fields.";
+        message.style.color = "red";
+    }
+});
